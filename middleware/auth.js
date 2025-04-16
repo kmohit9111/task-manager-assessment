4b3b9f2e78a80a3b1746adfb613743bfca1e8549
Mohit <kmohit9111@gmail.com>
2025-04-16 09:36:37 +0530

    error messages

--- middleware/auth.js
+++ middleware/auth.js
@@ -5,13 +5,20 @@ module.exports = function (req, res, next) {
   const token = req.header("Authorization")?.split(" ")[1];
 
   if (!token)
-    return res.status(401).json({ message: "No token, authorization denied" });
+    return res
+      .status(401)
+      .json({ message: "No token provided. Please login to continue." });
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = { id: decoded.id };
     next();
   } catch (err) {
-    res.status(401).json({ message: "Token is not valid" });
+    res
+      .status(401)
+      .json({
+        message:
+          "Your session has expired or token is invalid. Please login again.",
+      });
   }
 };
